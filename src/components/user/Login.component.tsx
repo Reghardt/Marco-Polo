0bef30625a87de9231792ad4abca273e6d7805c3
Reghardt <reghardt1000@gmail.com>
2022-10-28 09:42:46 +0200

    Version pump

--- src/components/user/Login.component.tsx
+++ src/components/user/Login.component.tsx
@@ -107,7 +107,7 @@ export default function Login()
             </Box>
 
             <Box sx={{marginTop: "1em"}}>
-              <Typography variant="body1" gutterBottom sx={{textAlign:'center'}} color='primary'>Experimental - Beta 0.13.1</Typography>
+              <Typography variant="body1" gutterBottom sx={{textAlign:'center'}} color='primary'>Experimental - Beta 0.13.2</Typography>
             </Box>
           </Stack>
 
